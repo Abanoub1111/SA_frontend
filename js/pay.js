9f46dfaf373f5ac373fa7aecdb6a8b40d44146d9
Abanoub1111 <120176396+Abanoub1111@users.noreply.github.com>
2023-12-11 18:40:16 +0200

    Add files via upload

---
+++ js/pay.js
@@ -0,0 +1,15 @@
+shipaddress.onclick = function () {
+    
+    con.style.display = "block";
+    visa.style.display="none";
+       
+
+}
+
+paypal.onclick = function () {
+    
+    visa.style.display = "block";
+    con.style.display="none";
+       
+
+}
